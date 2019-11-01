4021099d198592d7153d4b981f5213b981adc4a6
Naman Tiwari <namantiwari.nt@gmail.com>
2019-11-01 17:50:05 -0400

    Implement basic version of the network based on the paper.
    Add gitignore file. Only has swp for now.

---
+++ network.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+class model(nn.Module):
+    def __init__(self):
+        super(model, self).__init__()
+
+        ## 
+        # Linear layer with tansig activation.
+        #
+        # Input Size 12, one set of Postion, Velocity
+        # measurements from each of the 6 joints.
+        #
+        # Output Size 100
+        ##
+        self.layer1 = nn.Linear(12, 100, bias=True)
+
+        ##
+        # Linear Layer
+        # 
+        # Output Size 1, Torque estimate for this joint.
+        ##
+        self.layer2 = nn.Linear(100, 1, bias=True)
+
+
+    def forward(self, input):
+        layer1 = self.layer1(input)
+        layer1 = torch.tanh(layer1)
+
+        layer2 = self.layer2(layer1)
+
+        output = layer2
+
+        return output
+
