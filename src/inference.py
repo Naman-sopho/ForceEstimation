3cdc9104c90f37eb0461bf1cfd667291cdd81b91
Naman-sopho <namantiwari.nt@gmail.com>
2020-02-27 16:33:26 -0500

    Minor bug fix.

--- src/inference.py
+++ src/inference.py
@@ -38,7 +38,7 @@ def runInference(input_data, input_labels, input_file=None):
     correct = np.zeros((6, 1))
 
     predicted = np.empty((0, 6))
-    labels = np.empty(0, 6))
+    labels = np.empty((0, 6))
 
     for data, label in zip(input_data, input_labels):
         output1 = network1(data)
@@ -49,7 +49,7 @@ def runInference(input_data, input_labels, input_file=None):
         output6 = network6(data)
 
         predicted = np.append(predicted, np.array([[output1.item(), output2.item(), output3.item(), output4.item(), output5.item(), output6.item()]]), axis=0)
-        labels = np.append(labels, np.array([[label[0], label[1], label[2], label[3], label[4], label[5]]), axis=0)
+        labels = np.append(labels, np.array([[label[0], label[1], label[2], label[3], label[4], label[5]]]), axis=0)
 
         correct[0] = correct[0] + 1 if output1.item() - label[0] < ACC_DIFF else correct[0]
         correct[1] = correct[1] + 1 if output2.item() - label[1] < ACC_DIFF else correct[1]
